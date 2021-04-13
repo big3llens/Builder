012b3232c738535133ec4581f7cbbe4ae40b38f6
big3llens <zytgjvy.tuj1zytgjvy.tuj1>
2021-04-13 17:33:43 +0300

    add project

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,5 @@ public class App {
     public static void main(String[] args) {
         CarBuilder cb = new CarBuilder();
         Car car = cb.setEngine(new Engine(4, EnginesType.PETROL)).setTransmission(new Transmission()).setBodyType(BodyType.HATCHBACK).build();
-//        System.out.println(car);
     }
 }
